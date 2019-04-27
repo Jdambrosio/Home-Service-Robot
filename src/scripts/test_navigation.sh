5e3665ad82c3e185a129e4a48c5d8670ecc9e279
John D <jbrosio94@gmail.com>
2019-04-27 22:43:25 +0000

    type: feat
    Initial commit on Home Service Robot

---
+++ src/scripts/test_navigation.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/workspace/catkin_ws/src/World/my_world.world " &
+sleep 5
+xterm  -e  " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=/home/workspace/catkin_ws/src/maps/my_map.yaml " &
+sleep 5
+xterm  -e  " roslaunch turtlebot_rviz_launchers view_navigation.launch " &
+
